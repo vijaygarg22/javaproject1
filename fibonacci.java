4040570ce132022d322dd7e4a6f2da43be4bd8fd
vijaygarg22 <vijaygarg22@gmail.com>
2021-04-22 13:14:36 +0530

    First Java Project

---
+++ fibonacci.java
@@ -0,0 +1,12 @@
+class fibonacci{
+public static void main(String arg[])
+{
+int n1=0,n2=1,n3,i,count=15;
+System.out.print(n1+" "+n2);
+for( i=2;i<count;++i)
+{
+n3=n1+n2;
+System.out.print(" "+n3);
+n1=n2;
+n2=n3;
+}}}
